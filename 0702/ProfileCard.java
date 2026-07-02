2281851ea2b864992ccfd44d50e790b6e3965e53
special0714 <ray70106@gmail.com>
2026-07-02 12:46:12 +0800

    0702

---
+++ 0702/ProfileCard.java
@@ -0,0 +1,14 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        // 宣告名字科系與最愛課程
+        String name = "Amy";
+        String department = "Information Management";
+        String favoriteCourse = "Programming";
+
+        // 依序印出名片名字科系最愛課程
+        System.out.println("=== Profile ===");
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Favorite Course: " + favoriteCourse);
+    }
+}
